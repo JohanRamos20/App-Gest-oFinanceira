a8c6708b08c9c7f8deaeb180c54f794ee6a42ceb
Johan <johanrsilveira@gmail.com>
2026-06-03 10:39:21 -0300

    app e server

--- API/src/infrastructure/http/routes/usuario-router.ts
+++ API/src/infrastructure/http/routes/usuario-router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { makeUsuarioController } from '../factories/usuario-factory';
 
-const usuarioRouter = Router()
+const usuarioRoutes = Router()
 const usuarioController = makeUsuarioController()
 
-usuarioRouter.post('/usuarios', (req, res) => usuarioController.createUser(req, res));
-usuarioRouter.patch('/usuarios/:id/senha', (req, res) => usuarioController.updatePassword(req, res));
-usuarioRouter.get('/usuarios/:id/wallet', (req, res) => usuarioController.userWallet(req, res));
+usuarioRoutes.post('/usuarios', (req, res) => usuarioController.createUser(req, res));
+usuarioRoutes.patch('/usuarios/:id/senha', (req, res) => usuarioController.updatePassword(req, res));
+usuarioRoutes.get('/usuarios/:id/wallet', (req, res) => usuarioController.userWallet(req, res));
 
-export { usuarioRouter };
+export { usuarioRoutes };
---
+++ API/src/main/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { usuarioRoutes } from "../infrastructure/http/routes/usuario-router";
+
+const app = express();
+
+app.use(express.json());
+app.use(usuarioRoutes);
+
+export { app }
