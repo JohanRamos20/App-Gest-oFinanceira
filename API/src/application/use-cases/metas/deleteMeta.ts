a6f1da1ddbd24e913d6145a003087bad6c2c3738
Johan <johanrsilveira@gmail.com>
2026-06-02 22:17:51 -0300

    deleteMeta-UseCase

---
+++ API/src/application/use-cases/metas/deleteMeta.ts
@@ -0,0 +1,21 @@
+import { Meta } from "../../../domain/entities/meta";
+import { MetasRepository } from "../../../domain/repository/meta-repository";
+
+export interface DeleteMetaRequest {
+    id_meta: string;
+}
+
+class DeleteMeta {
+    constructor(private metaRepository: MetasRepository) {}
+
+    async delete (req: DeleteMetaRequest) : Promise<void> {
+        const metaDelete = await this.metaRepository.findMetaByID(req.id_meta);
+
+        if (!metaDelete) {
+            throw new Error("Meta não encontrada");
+        }
+
+        await this.metaRepository.deleteMeta(req.id_meta);
+
+    }
+}
