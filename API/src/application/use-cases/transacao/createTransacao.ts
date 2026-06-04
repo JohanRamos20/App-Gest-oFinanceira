33d00e4c48b9de502636c2b1730a5b6f5d067ac2
Johan <johanrsilveira@gmail.com>
2026-06-04 10:46:50 -0300

    transacao-controller

--- API/src/application/use-cases/transacao/createTransacao.ts
+++ API/src/application/use-cases/transacao/createTransacao.ts
@@ -2,6 +2,7 @@ import { TransacaoRepository } from "../../../domain/repository/transacao-reposi
 import { CarteiraRepository } from "../../../domain/repository/carteira-repository";
 import { UsuarioRepository } from "../../../domain/repository/usuario-repository";
 import { Transacao, Categorias, TipoTransacao } from "../../../domain/entities/transacao";
+import { toTransacaoDto, TransacaoDto } from "../../dtos/transacao-dtos";
 
 export interface CreateTransacaoRequest {
     nome: string;
@@ -11,13 +12,13 @@ export interface CreateTransacaoRequest {
     tipo_transacao: TipoTransacao;
 }
 
-class CreateTransacao {
+export class CreateTransacaoUseCase {
     constructor(private transacaoRepository: TransacaoRepository
         , private usuarioRepository: UsuarioRepository,
         private carteiraRepository: CarteiraRepository
     ) {}
 
-    async create(req: CreateTransacaoRequest) : Promise<Transacao> {
+    async create(req: CreateTransacaoRequest) : Promise<TransacaoDto> {
         const carteiraExistente = await this.carteiraRepository.getByUserId(req.id_carteira);
         if(!carteiraExistente) {
             throw new Error("Carteira não encontrada");
@@ -31,7 +32,7 @@ class CreateTransacao {
         tipo_transacao: req.tipo_transacao
     });
 
-    const transacaoCreate = await this.transacaoRepository.createTransacao(transacao);
+    const transacaoCriada = await this.transacaoRepository.createTransacao(transacao);
 
     const todasTransacoes = await this.transacaoRepository.getAllTransacaoByCarteira(req.id_carteira);
     const novoSaldo = todasTransacoes.reduce((saldo, t) => {
@@ -42,6 +43,6 @@ class CreateTransacao {
 
     await this.carteiraRepository.setCacheWalletBalance(req.id_carteira, novoSaldo);
 
-    return transacaoCreate;
+    return toTransacaoDto(transacaoCriada);
 }
 }
---
+++ API/src/infrastructure/http/controller/transacao-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { type CreateTransacaoUseCase } from "../../../application/use-cases/transacao/createTransacao";
+
+export interface TransacaoUseCases {
+    createTransacao: CreateTransacaoUseCase;
+}
+
+export class TransacaoController {
+    constructor(private readonly transacaoUseCases: TransacaoUseCases) {}
+
+    async createTransacao(req: Request, res: Response): Promise<void> {
+        try{
+            const transacao = await this.transacaoUseCases.createTransacao.create(req.body)
+            res.status(201).json(transacao);
+        }
+        catch (error) {
+            res.status(400).json({error})
+        }
+    }
+}
